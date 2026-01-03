2df7853ddb91db0d4e087235a937487e74507869
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-03 20:48:49 +0530

    Initial commit

---
+++ 10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+// arry
+    int luckyNums[20] = {1, 5, 6, 7, 8, 7, 11, 23};
+   
+    luckyNums[5] = 19;
+
+    cout << luckyNums[5];
+
+
+    return 0;
+}
