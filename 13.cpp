a87ec527f181ae3f93cbcabd6be3d9c21e329980
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-05 20:53:16 +0530

    Initial commit

---
+++ 13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int getMax (int num1, int num2){
+    int result;
+
+    if(num1 < num2){
+        result = num1;
+    }
+    else {
+        result = num2;
+    }
+    return result;
+}
+
+int main(){
+    cout << getMax(4, 5);
+
+    return 0;
+}
