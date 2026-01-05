05201dce984835b1b8753d2c4813c9cdc43fecfa
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-05 20:52:38 +0530

    Initial commit

---
+++ 12.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    bool isMale = true;
+    bool isTall = true;
+
+    if (isMale && isTall) {
+        cout <<"you are a tall male";
+    }
+
+    else if(isMale && !isTall){
+        cout <<"you are a short male";
+    }
+
+    else if (!isMale && isTall){
+        cout <<"you are tall but not male";
+    }
+
+    else {
+        cout << "you are not male and not tall"; 
+    }
+
+
+    return 0;
+}
