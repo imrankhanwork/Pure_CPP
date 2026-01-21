812d63a33ff242620a3edd1d7a35eac17f5cc48f
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-21 20:38:42 +0530

    Initial commit

---
+++ 03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string chname ="asd";
+    int chage;
+    chage = 20;
+
+    cout <<"asd "<< chname << endl;
+    cout <<"asd "<< chname << endl;
+    cout <<"dkjsndkjn "<< chage << " dkjshdfkh"<< endl;
+    cout <<"20 "<< chage << endl;
+    return 0;
+}
