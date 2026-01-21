fb512480e2881a1051c7196fa7e8fb25aee6bcc1
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-21 20:40:43 +0530

    Initial commit

---
+++ 05.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string phrase = "the name is  imran";
+    cout<< phrase.length()<<endl;
+
+    cout << "name"<< endl;
+
+    cout << phrase[0];
+    cout << phrase[1];
+    cout << phrase[2];
+    cout << phrase[3];
+    cout << phrase[4];
+    cout << phrase[5];
+    cout << phrase[6];
+    cout << phrase[7];
+    cout << phrase[8];
+    cout << phrase[9];
+    cout << phrase[10];
+    
+    cout << phrase[12]/n;
+    cout << phrase[13]<<endl;
+    cout << phrase[14]<<endl;
+    cout << phrase[15]<<endl;
+    cout << phrase[16]<<endl;
+    cout << phrase[17]<<endl;
+
+    phrase[12]='r';
+    phrase[13]='e';
+    phrase[14]='h';
+    phrase[15]='a';
+    phrase[16]='n';
+    phrase[17]='a';
+    cout << phrase ;
+
+    return 0; 
+}
