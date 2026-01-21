9e50dbcc1e14cf0082b7e7751c0787ee1d5ca683
imrankhanwork <imrankhanwork20@gmail.com>
2026-01-21 20:39:26 +0530

    Initial commit

---
+++ 04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char grade ='d';
+    string phrase = "imran is cool";
+    int age = 50;
+    float a = 1.0;
+    double gpa = 2.3333333;
+    bool ismale = true;
+    cout <<grade<< endl;
+    return 0;
+}
